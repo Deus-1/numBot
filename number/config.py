eb917d3049e2a9d12a4093dbb7ac90afa87178b6
Deus-1 <47428999+Deus-1@users.noreply.github.com>
2019-04-03 22:34:54 +0300

    number commit

---
+++ number/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+token = '870851572:AAG9wb9F9ybAWrqUA382tcRoUk72oZ5Ezps'
+
+database_name = 'numbers.db'
+database_table = 'players'
+database_table_columns = ['chat_id', 'max_score', 'game_start', 'game_time', 'game_right_answer',
+                          'game_score', 'game_complexity', 'game_left_seed', 'game_right_seed']
+
+settings = { 'easy': [5, 10, 15, 5],
+             'medium': [10, 15, 10, 10],
+             'hard': [15, 20, 5, 15] }
